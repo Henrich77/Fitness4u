64aa6cea659f27a1830ceed1cb3690259e08a029
NEO <qj82189@gmail.com>
2023-02-13 13:58:20 -0500

    Gave us views for testing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -72,7 +72,7 @@ router.get('/profile', async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('homepage');
+    res.redirect('/profile');
     return;
   }
 
